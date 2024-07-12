add6062f68f7ca05445f722ad0c9e7aae7119182
Meltasy <119603631+Meltasy@users.noreply.github.com>
2024-07-12 11:22:40 +0200

    Update and add functions to use bookshelf
    Add function to add book to bookshelf
    Add function to clear and refill bookshelf

--- script.js
+++ script.js
@@ -1,74 +1,95 @@
-// const myLibrary = [];
+let myLibrary = [
+    new Book("The Garden of Evening Mists", "Tan Twan Eng", "historical fiction", 348, true),
+    new Book("The Gift of Rain", "Tan Twan Eng", "historical fiction", 508, true),
+    new Book("Au revoir là-Haut", "Pierre Lemaitre", "historical fiction", 624, true),
+    new Book("Le grand monde", "Pierre Lemaitre", "historical fiction", 756, false),
+    new Book("The Three-Body Problem", "Ci Xin Liu", "science-fiction", 424, true),
+    new Book("The Dark Forest", "Ci Xin Liu", "science-fiction", 550, true),
+    new Book("Un si bel horizon", "Françoise Bourdin", "fitcion", 285, false),
+    new Book("Face à la mer", "Françoise Bourdin", "fitcion", 334, false),
+];
 
-// function Book(title, author, genre, numPages, isRead) {
-//     this.title = title;
-//     this.author = author;
-//     this.genre = genre;
-//     this.numPages = numPages;
-//     this.isRead = function() {
-//         if (isRead === true) {
-//             return "This books has been read.";
-//         } else if (isRead === false) {
-//             return "This book is waiting to be read. Quick, pick it up!";
-//         }
-//     }
-//     this.info = function() {
-//         return `${this.title} by ${this.author} has ${numPages} pages and is a ${genre} story.`;
-//     }
-// }
+// let getNewBook = document.querySelector(".getNewBook");
 
-// function addBookToLibrary(title, author, genre, numPages, isRead) {
-//     let newBook = new Book(title, author, genre, numPages, isRead);
-//     myLibrary.push(new Book);
-//     return newBook;
-// }
+// getNewBook.addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     let newBook = new Book ({
+//         id: newId,
+//         title: getTitle.value,
+//         author: getAuthor.value,
+//         genre: getGenre.value,
+//         numPages: getNumPages.value,
+//         isRead: getIsRead.value,
+//     });
+// });
 
-// const book1 = new Book("The Garden of Evening Mists", "Tan Twan Eng", "historical fiction", 348, true);
-// const book2 = new Book("The Gift of Rain", "Tan Twan Eng", "historical fiction", 508, true);
-// const book3 = new Book("Au revoir là-Haut", "Pierre Lemaitre", "historical fiction", 624, true);
-// const book4 = new Book("Le grand monde", "Pierre Lemaitre", "historical fiction", 756, false);
-// const book5 = new Book("The Three-Body Problem", "Ci Xin Liu", "science-fiction", 424, true);
-// const book6 = new Book("The Dark Forest", "Ci Xin Liu", "science-fiction", 550, true);
-// const book7 = new Book("Un si bel horizon", "Françoise Bourdin", "fitcion", 285, false);
-// const book8 = new Book("Face à la mer", "Françoise Bourdin", "fitcion", 334, false);
+// let myLibrary = [];
 
-const bookshelf = document.querySelector("#bookshelf");
-console.log(bookshelf);
-
-function createBook(title, author, genre, numPages, isRead) {
-    const book = document.createElement("div");
-    book.className = "card";
-
-    const titleEntry = document.createElement("h3");
-    titleEntry.textContent = `Title: ${title}`;
-
-    const authorEntry = document.createElement("p");
-    authorEntry.textContent = `Author: ${author}`;
-
-    const genreEntry = document.createElement("p");
-    genreEntry.textContent = `Genre: ${genre}`;
-
-    const pageEntry = document.createElement("p");
-    pageEntry.textContent = `${numPages} pages`;
-
-    const readEntry = document.createElement("p");
-    readEntry.textContent = isRead;
-
-    book.appendChild(titleEntry);
-    book.appendChild(authorEntry);
-    book.appendChild(genreEntry);
-    book.appendChild(pageEntry);
-    book.appendChild(readEntry);
-
-    return book;
+function Book(title, author, genre, numPages, isRead) {
+// function Book(id, title, author, genre, numPages, isRead) {
+    // this.id = id;
+    this.title = title;
+    this.author = author;
+    this.genre = genre;
+    this.numPages = numPages;
+    this.isRead = function() {
+        if (isRead === true) {
+            return "This books has been read.";
+        } else if (isRead === false) {
+            return "This book is waiting to be read. Quick, pick it up!";
+        }
+    }
+    this.info = function() {
+        return `${this.title} by ${this.author} has ${numPages} pages and is a ${genre} story.`;
+    }
 }
 
-bookshelf.appendChild(createBook("Wild Swans", "Jung Chang", "historical fiction", 670, true));
-bookshelf.appendChild(createBook("The Garden of Evening Mists", "Tan Twan Eng", "historical fiction", 348, true));
-bookshelf.appendChild(createBook("The Gift of Rain", "Tan Twan Eng", "historical fiction", 508, true));
-bookshelf.appendChild(createBook("Au revoir là-Haut", "Pierre Lemaitre", "historical fiction", 624, true));
-bookshelf.appendChild(createBook("Le grand monde", "Pierre Lemaitre", "historical fiction", 756, false));
-bookshelf.appendChild(createBook("The Three-Body Problem", "Ci Xin Liu", "science-fiction", 424, true));
-bookshelf.appendChild(createBook("The Dark Forest", "Ci Xin Liu", "science-fiction", 550, true));
-bookshelf.appendChild(createBook("Un si bel horizon", "Françoise Bourdin", "fitcion", 285, false));
-bookshelf.appendChild(createBook("Face à la mer", "Françoise Bourdin", "fitcion", 334, false));
+function addBookToLibrary() {
+    // let id = myLibrary.forEach(getId);
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const genre = document.querySelector("#genre").value;
+    const numPages = document.querySelector("#numPages").value;
+    const isRead = document.querySelector("input[name=isRead]:checked").value;
+    // let newBook = new Book(id, title, author, genre, numPages, isRead);
+    const newBook = new Book(title, author, genre, numPages, isRead);
+    myLibrary.push(newBook);
+    updateBookshelf();
+}
+
+function updateBookshelf() {
+    let bookshelf = document.querySelector("#bookshelf");
+    while (bookshelf.firstChild) {
+        bookshelf.removeChild(bookshelf.lastChild);
+    }
+    for (i = 0; i < myLibrary.length; i++) {
+        createBook(myLibrary[i]);
+    }
+}
+
+function createBook(newBook) {
+    let bookshelf = document.querySelector("#bookshelf");
+    let eachBook = document.createElement("div");
+    // const id = document.createElement("p");
+    // id.textContent = `${newBook.id}`;
+    let title = document.createElement("h3");
+    title.textContent = `${newBook.title}`;
+    let author = document.createElement("h4");
+    author.textContent = `by ${newBook.author}`;
+    let genre = document.createElement("p");
+    genre.textContent = `${newBook.genre}`;
+    let numPages = document.createElement("p");
+    numPages.textContent = `${newBook.numPages} pages`;
+    let isRead = document.createElement("p");
+    isRead.textContent = `${newBook.isRead()}`;
+    eachBook.appendChild(title);
+    eachBook.appendChild(author);
+    eachBook.appendChild(genre);
+    eachBook.appendChild(numPages);
+    eachBook.appendChild(isRead);
+    bookshelf.appendChild(eachBook);
+}
+
+updateBookshelf();
+
+console.log(myLibrary);
